f4b66402441b069b0da1f5a5470c3c3a5a292231
shosatojp <me@shosato.jp>
2020-08-04 18:41:26 +0900

    pypi 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'rt', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='dlimg',
-    version='0.1.5',
+    version='0.1.8',
     author='shosatojp',
     author_email='me@shosato.jp',
     description='Fast image downloader',
@@ -26,5 +26,6 @@ setuptools.setup(
         'bs4',
         'aiofiles',
         'selenium',
+        'lxml'
     ],
 )
