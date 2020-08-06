68ac2d04f47738efd9e5d1e8398f65f0049f0c9e
shosatojp <me@shosato.jp>
2020-08-06 12:16:58 +0900

    update test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./dlimg --site yandere --query miyase_mahiro -c 10 -o bin/yandere -SU --name random
-./dlimg --site yandere --query miyase_mahiro -c 10 -o bin/yandere -SU --name keep
+# ./dlimg --site yandere --query miyase_mahiro -c 10 -o bin/yandere -SU --name random
+# ./dlimg --site yandere --query miyase_mahiro -c 10 -o bin/yandere -SU --name keep
 
 ./dlimg --site google --query dog -c 10 -o bin/google
 ./dlimg --site bing --query cat -c 10 -o bin/bing
@@ -10,6 +10,9 @@
 
 ./dlimg --site unsplash --query 'river' -c 10 -o bin/unsplash
 
-./dlimg https://wear.jp/user/ --waitlist wait.json -o bin/wear_user -SU --name keep -w 5 -HL 3 -LL 2 -H /home/sho/repos/fast-image-downloader/slack.sh -ps 1 -pe 1 -c 10
+if [[ ! -f wait.json ]];do
+    echo '{ "wear.jp": 5, "cdn.wimg.jp": 1, "www.google.com": "random 1 4"}' > wait.json
+done
+./dlimg https://wear.jp/user/ --waitlist wait.json -o bin/wear_user -SU --name keep -w 5 -LL 2 -ps 1 -pe 1 -c 10
 
-./dlimg --site tsundora --query '犬' -c 10 -o bin/tsundora
+# ./dlimg --site tsundora --query '犬' -c 10 -o bin/tsundora
