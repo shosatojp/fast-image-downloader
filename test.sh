4cccc88ff0b27c865dd6b62ce7c38b90f99e322b
shosatojp <me@shosato.jp>
2020-08-06 12:27:15 +0900

    :+1: fix test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# ./dlimg --site yandere --query miyase_mahiro -c 10 -o bin/yandere -SU --name random
-# ./dlimg --site yandere --query miyase_mahiro -c 10 -o bin/yandere -SU --name keep
-
 if [[ ! -f wait.json ]];then
     echo '{ "wear.jp": 5, "cdn.wimg.jp": 1, "www.google.com": "random 1 4"}' > wait.json
 fi
@@ -12,5 +9,3 @@ fi
 ./dlimg --site irasutoya --query '犬' -c 10 -o bin/irasutoya && \
 ./dlimg --site unsplash --query 'river' -c 10 -o bin/unsplash && \
 ./dlimg https://wear.jp/user/ --waitlist wait.json -o bin/wear_user -SU --name keep -w 5 -LL 2 -ps 1 -pe 1 -c 10
-
-# ./dlimg --site tsundora --query '犬' -c 10 -o bin/tsundora
